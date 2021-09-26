7f00be9ddd10e647ed441df9425226a40d2efd14
Kasper Mennink <kasper.mennink@hotmail.com>
2021-09-26 22:50:42 +0200

    Added inputfields.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,7 @@
+const pushBook = document.getElementById("addBook");
+
+let myLibrary = [];
+
 function Book(title, author, pages, read) {
 this.title = title
 this.author = author
@@ -9,4 +13,19 @@ return this.title + " " + "by " + this.author + ", " + this.pages + ", " + this.
 };
 
 const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295 pages', 'not read yet')
-console.log(theHobbit.info());
+
+
+function addBooktoLibrary() {
+    let title = document.getElementById("titleName").value;
+    let author = document.getElementById("authorName").value;
+    let pages = document.getElementById("numberOfPages").value;
+    let read = document.getElementById("readYet").value;
+const newBook = new Book(title, author, pages, read);
+myLibrary.push(newBook);
+console.log(myLibrary);
+};
+
+pushBook.addEventListener('click', addBooktoLibrary());
+pushBook.addEventListener('click', function(event) {
+    event.preventDefault();
+});
