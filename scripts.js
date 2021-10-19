b8dbfcc1f0fca4007ae96f35dd98ae39d1595ff5
Kasper Mennink <kasper.mennink@hotmail.com>
2021-10-19 22:54:23 +0200

    Finished it up before initial feedback from TOP community.

--- scripts.js
+++ scripts.js
@@ -1,70 +1,82 @@
+//Globals
 const submitButton = document.querySelector("#addBook");
 const userInput = document.querySelector(".bookForm");
 const inputs = document.querySelectorAll('input');
 const bookShelf = document.querySelector('#bookshelf');
 
-
+//Array
 let myLibrary = [];
 
+//Constructor
 function Book(title, author, pages, read) {
-this.title = title
-this.author = author
-this.pages = pages
-this.read = read
-this.info = function() {
-return this.title + " " + "by " + this.author + ", " + this.pages + ", " + this.read;
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+    this.info = function() {
+    return this.title + " " + "by " + this.author + ", " + this.pages + ", " + this.read;
 };
 };
 
+//Example from exercise
 const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295 pages', 'not read yet')
 
-
+//Functions
 function addBooktoLibrary() {
-let newBook = new Book(
+    let newBook = new Book(
     document.querySelector("#titleName").value,
     document.querySelector("#authorName").value,
     document.querySelector("#numberOfPages").value,
     document.querySelector("#readYet").value,
 );
-myLibrary.push(newBook);
-console.log(myLibrary);
+    myLibrary.push(newBook);
+    setLocalStorage();
+    console.log(myLibrary);
 };
 
 function displayLibrary() {
-bookShelf.innerHTML = ''; 
-myLibrary.forEach(book => {
-const novel = document.createElement('div');
-const removeButton = document.createElement('button');
-const readButton = document.createElement('button');
+    bookShelf.innerHTML = ''; 
+    myLibrary.forEach(book => {
+    const bookToAdd = document.createElement('div');
+    const removeButton = document.createElement('button');
+    const readButton = document.createElement('button');
 
-novel.classList.add('addedBook');
-novel.textContent = `Title: ${book.title} \r\n 
-Author: ${book.author} \r\n 
-Number of pages: ${book.pages} \r\n`;
+    bookToAdd.classList.add('addedBook');
+    bookToAdd.textContent = `Title: ${book.title} \r\n 
+    Author: ${book.author} \r\n 
+    Number of pages: ${book.pages} \r\n`;
 
-readButton.classList.add('readBtn');
-if(book.read===false) {
+    readButton.classList.add('readBtn');
+     if(book.read===false) {
     readButton.textContent = 'Not read';
     readButton.style.background = '#B22222';
-} else {
+}      else {
     readButton.textContent = 'Read';
     readButton.style.background = '#006400';
 };
-readButton.addEventListener('click', () => {
+    readButton.addEventListener('click', () => {
     book.read = !book.read;
     displayLibrary();
+    setLocalStorage();
 });
 
-removeButton.classList.add('removeBtn');
-removeButton.textContent = 'Remove';
-removeButton.addEventListener('click', deleteBook);
+    removeButton.classList.add('removeBtn');
+    removeButton.textContent = 'Remove';
+    removeButton.addEventListener('click', deleteBook);
 
-novel.appendChild(readButton);
-novel.appendChild(removeButton);
-bookShelf.appendChild(novel); 
+    bookToAdd.appendChild(readButton);
+    bookToAdd.appendChild(removeButton);
+    bookShelf.appendChild(bookToAdd); 
 });
 };
 
+function deleteBook(e) {
+    e.target.parentElement.remove();
+    myLibrary.splice(e.target, 1);
+    setLocalStorage();
+};
+
+//Submit 
 submitButton.addEventListener('click', (e) => {
     e.preventDefault();
     addBooktoLibrary();
@@ -72,7 +84,19 @@ submitButton.addEventListener('click', (e) => {
     inputs.forEach(input => input.value = '');
 });
 
-function deleteBook(e) {
-e.target.parentElement.remove();
-myLibrary.splice(e.target, 1);
+//Local storage functions
+
+function setLocalStorage() {
+    localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
+};
+
+function getLocalstorage() {
+    if(!localStorage.myLibrary) {
+    displayLibrary();
+} else {
+    let savedLibrary = JSON.parse(localStorage.getItem('myLibrary'));
+    myLibrary = savedLibrary;
+    displayLibrary();
 }
+};
+getLocalstorage();
